fc186e8a0825f5f16e5e0fcbc8fe3a74e9f9e149
ajzepeda <ajzepeda@email.com>
2023-04-06 09:29:37 -0600

    Program Complete

--- Chapter_3/guest_list_3_4.py
+++ Chapter_3/guest_list_3_4.py
@@ -1,1 +1,19 @@
 
+
+''' 3-4. Guest List: If you could invite anyone, living or deceased, to dinner, who
+would you invite? Make a list that includes at least three people you’d like to
+invite to dinner. Then use your list to print a message to each person, inviting
+them to dinner. '''
+
+guest = input('Enter the name of the guest 1: \n')
+guest_list = []
+i = 1 # flag value
+while guest != '-1':
+    i+=1
+    guest_list.append(guest)
+    guest = input(f'Enter the name of the guest {i}: \n')
+
+print(guest_list)
+
+
+

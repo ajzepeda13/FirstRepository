21388021c6d33b2e2a088fe54c78a8bc4de34be6
ajzepeda <ajzepeda@email.com>
2023-04-16 09:29:00 -0600

    Limit list to maximu 10 guest using and operator

--- Chapter_3/changing_guest_list_3_5.py
+++ Chapter_3/changing_guest_list_3_5.py
@@ -4,6 +4,7 @@
 
 You just heard one of your guests can't make the dinner, so you need to send out a new set of invitations. You'll have to think of someone else to invite
 
+
 """
 
 guest = input('Enter the name of the guest 1: \n')
--- Chapter_3/guest_list_3_4.py
+++ Chapter_3/guest_list_3_4.py
@@ -1,21 +1,33 @@
 
 
-''' 3-4. Guest List: If you could invite anyone, living or deceased, to dinner, who
+''' 
+Alejandro Z -> @
+
+3-4. Guest List: If you could invite anyone, living or deceased, to dinner, who
 would you invite? Make a list that includes at least three people you’d like to
 invite to dinner. Then use your list to print a message to each person, inviting
-them to dinner. '''
+them to dinner. 
+
+'''
+
+'''
+1. Limit list to maximum 10 guests
+
+
+'''
 
 guest = input('Enter the name of the guest 1: \n')
 guest_list = []
-i = 1 # flag value
+i = 1 # Counter
 
 while guest == '-1':
     guest = input('Please enter the name of guest 1 (you need at least one guess): \n')
 
-while guest != '-1':
+while guest != '-1' and i < 11:
     i+=1
     guest_list.append(guest)
     guest = input(f'Enter the name of the guest {i} (when finish type: -1): \n')
+    print(i)
 
 print(guest_list)
 

c10f2b9ce45a5de53e4f813852b5abaf3dc51e7d
ajzepeda <ajzepeda@email.com>
2023-05-27 09:20:48 -0600

    Completed 4-2 Animals Exercise

--- Chapter 4/program1.py
+++ Chapter 4/program1.py
@@ -23,4 +23,21 @@ print()
 print('Those were my favorite types of pizza')
 print()
 
+"""
+4-2. Animals: Think of at least three different animals that have a common characteristic.
+Store the names of these animals in a list, and then use a for loop to
+print out the name of each animal.
+• Modify your program to print a statement about each animal, such as
+A dog would make a great pet.
+• Add a line at the end of your program stating what these animals have in
+common. You could print a sentence such as Any of these animals would
+make a great pet!
+"""
 
+common_animals = ['Lion', 'Tiger', 'Cat', 'Puma', 'Panther']
+print()
+for animal in common_animals:
+    print(animal)
+
+print()
+print('Those were similar animals!')
